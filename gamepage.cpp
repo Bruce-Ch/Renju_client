723d76656085e0d8159dd6548f162a091650f647
Bruce <1178334271@qq.com>
2020-07-13 11:41:24 +0800

    feat: modify the port
    Change the port from 9999 to 10086 so that old version cannot connect to the server.
    
    BREAKING CHANGE: now the port is 10086

--- gamepage.cpp
+++ gamepage.cpp
@@ -18,7 +18,7 @@ GamePage::GamePage(QWidget *parent)
     connect(this, &GamePage::timeToAbort, this, &GamePage::abort);
     client = new QTcpSocket(this);
     //client->connectToHost("127.0.0.1", 8899);
-    client->connectToHost("39.106.78.242", 9999);
+    client->connectToHost("39.106.78.242", 10086);
     connect(client, &QTcpSocket::readyRead, this, &GamePage::implementMessage);
     connect(client, &QTcpSocket::disconnected, this, &GamePage::loseConnection);
 }
